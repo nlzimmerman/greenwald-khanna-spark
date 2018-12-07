79d85399e137cd45df05f2ee79ce20948c6dd1fe
Neil L Zimmerman <nlz@nlz.me>
2018-12-07 16:58:34 -0500

    combine is implemented but totally untested

--- src/main/scala/com/github/nlzimmerman/GreenwaldKhanna.scala
+++ src/main/scala/com/github/nlzimmerman/GreenwaldKhanna.scala
@@ -1,16 +1,20 @@
 package com.github.nlzimmerman
 
+import collection.mutable.ListBuffer
+
 // For now this is a line-for-line rewrite of what I did in Python;
 // Then I'll move it into a class
 
-class GKEntry(val v: Double, var g: Long, val delta: Long) {
+class GKEntry(val v: Double, var g: Long, var delta: Long) {
   override def toString: String = s"($v, $g, $delta)"
+  def copy(): GKEntry = new GKEntry(v, g, delta)
 }
 
-class GKRecord(val epsilon: Double) {
-  val sample: collection.mutable.ListBuffer[GKEntry] =
-    new collection.mutable.ListBuffer[GKEntry]
-  var count: Int = 0
+class GKRecord(
+  val epsilon: Double,
+  val sample: ListBuffer[GKEntry] = new ListBuffer[GKEntry],
+  var count: Long = 0,
+) {
   val compressThreshold: Long = (1.0/(2.0*epsilon)).toLong
 
   def insert(v: Double): Unit = {
@@ -79,6 +83,67 @@ class GKRecord(val epsilon: Double) {
       }
     }
     toReturn
+  }
+
+  def combine(that: GKRecord): GKRecord = {
+    if (this.sample.length == 0) that
+    else if (that.sample.length == 0) this
+    else {
+      val thisSample: ListBuffer[GKEntry] = sample.clone
+      val thatSample: ListBuffer[GKEntry] = that.sample.clone
+      val out: ListBuffer[GKEntry] = new ListBuffer[GKEntry]
+      while (thisSample.length > 0 && thatSample.length > 0) {
+        // This could be much, much better.
+        out += {
+          val (thisElement: GKEntry, otherNextElement: GKEntry) = {
+            if (thisSample(0).v < thatSample(0).v) {
+              // I'm not sure if it's safe to not copy this in Spark.
+              // it should be fine in straight Scala.
+              val a: GKEntry = thisSample(0).copy
+              thisSample.remove(0)
+              val b: GKEntry = thatSample.find(
+                (x) => x.v > a.v
+              ).get // will throw an error if there is none. That is correct
+              (a, b)
+            } else {
+              val a: GKEntry = thatSample(0).copy
+              thisSample.remove(0)
+              val b: GKEntry = thisSample.find(
+                (x) => x.v > a.v
+              ).get
+              (a, b)
+            }
+          }
+          val newDelta: Long = thisElement.delta + otherNextElement.delta +
+            otherNextElement.g - 1
+          thisElement.delta = newDelta
+          thisElement
+        }
+      } // either thisSample or thatSample has been exhausted now
+      // again, I'm of the impression that it's NOT safe to NOT copy
+      // thisSample and thatSample and just do a ++=
+      // which is why I'm doing this.
+      while (thisSample.length > 0) {
+        out += thisSample(0).copy
+        thisSample.remove(0)
+      }
+      while (thatSample.length > 0) {
+        out += thatSample(0).copy
+        thatSample.remove(0)
+      }
+      val newEpsilon: Double = math.max(epsilon, that.epsilon)
+      val countIncrease: Long = math.min(count, that.count)
+      val newCount: Long = count + that.count
+      val toReturn: GKRecord = new GKRecord(
+        newEpsilon,
+        out,
+        newCount
+      )
+      if (countIncrease >= math.floor(1.0/(2.0*newEpsilon))) {
+        toReturn.compress
+      }
+      toReturn
+    }
   }
 }
 
