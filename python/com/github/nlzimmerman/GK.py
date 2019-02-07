855167924e8de21e024c44d39317bc0d21759279
Neil L Zimmerman <nlz@nlz.me>
2019-02-07 12:37:14 -0500

    passing (slightly rewritten) unit tests

--- python/com/github/nlzimmerman/GK.py
+++ python/com/github/nlzimmerman/GK.py
@@ -3,184 +3,156 @@ from pyspark.mllib.common import _java2py, _py2java
 from pyspark.sql.column import Column, _to_java_column, _to_seq
 from pyspark.sql.functions import col
 from pyspark import SparkContext
-from pyspark.sql import SparkSession
+
 
 import json
 
-# no longer a singleton because we have some unavoidable install-specific configuration
+# Formerly, this lived in a singleton class, but I see that pyspark.sql.functions
+# puts its functions directly into the package. I'm emulating that pattern here.
 
-class GKQuantile(object):
+def java2py(x):
+    sc = SparkContext._active_spark_context
+    return _java2py(sc, x)
 
-    def __init__(
-        self,
-        #sparkSession
-    ):
-        #self.sparkSession = SparkSession._activeSession
-        self.sparkContext = SparkContext._active_spark_context
-        #self.sparkSession = sparkSession
+def py2java(x):
+    sc = SparkContext._active_spark_context
+    return _py2java(sc, x)
 
-
-    # def spark(self):
-    #     return (
-    #             SparkSession.
-    #             builder.
-    #             master(self.master).
-    #             appName(self.app_name).
-    #             config("spark.jars", self.jar_path).
-    #             getOrCreate()
-    #         )
-
-    def java2py(self, x):
-        return _java2py(self.sparkContext, x)
-
-    def py2java(self, x):
-        return _py2java(self.sparkContext, x)
-
-    # just for debugging
-    # do remove this at some point
-    def scalaAdd(self, x, y):
-        s = self.sparkContext._jvm.com.github.nlzimmerman.Python.add
-        return s(self.py2java(x), self.py2java(y))
-
-    # this method is for the use case where you have a spark SQL workflow
-    # in place, you use df.groupBy().agg() all the time, and you aren't
-    # interested in instantiating this class.
-    # This is the way I would tend to do it at work.
-    # This function returns a function that can be used with .agg()
-    @staticmethod
-    def gk_agg(
-        # sc, # this is a SPARK CONTEXT, not a Spark Session,.
-        #     # we have to have the spark context so we can find the function
-        #     # on the JVM
-        quantiles, # quantiles to find,
-        epsilon, # precision
-    ):
-        sc = SparkContext._active_spark_context
-        q = _py2java(sc, quantiles)
-        e = _py2java(sc, epsilon)
-        def _gk(col):
-            UntypedGKAggregator_instance = (
-                sc._jvm.com.github.nlzimmerman.UntypedGKAggregator(q, e)
-            )
-            instance_applier = UntypedGKAggregator_instance.apply
-            return Column(
-                instance_applier(
-                    _to_seq(
-                        sc,
-                        [col],
-                        _to_java_column
-                    )
+# This is likely to be the entry-point into this package you want.
+# Use it when you have a spark dataframe where you want to use
+# df.groupBy().agg()
+# for example, if you have a df(["name", "value"]),
+# you would run
+# df.groupBy(col("name")).agg(approximateQuantile([0.5], 0.05)(col("value")))
+# to get the median.
+def approximateQuantile(
+    quantiles,    # quantiles to find, as a python list full of doubles 0 <= x <= 1
+    epsilon=0.01, # precision, as a double. The default is likely to be good for you.
+):
+    sc = SparkContext._active_spark_context
+    q = _py2java(sc, quantiles)
+    e = _py2java(sc, epsilon)
+    # this page is a great reference for this
+    # http://www.cyanny.com/2017/09/15/spark-use-scala-udf-udaf-in-pyspark/
+    # Our case is kind of interesting because the UDAF itself takes parameters.
+    def _gk(col):
+        UntypedGKAggregator_instance = (
+            sc._jvm.com.github.nlzimmerman.UntypedGKAggregator(q, e)
+        )
+        instance_applier = UntypedGKAggregator_instance.apply
+        return Column(
+            instance_applier(
+                _to_seq(
+                    sc,
+                    [col],
+                    _to_java_column
                 )
             )
-        return _gk
-
-    def approximateQuantile(self, quantiles, epsilon):
-        return self.gk_agg(quantiles, epsilon)
-
-    def getGroupedQuantilesSQL(
-        self,
-        df, # DF with some key column that responds to GroupBy
-           # and some value column that is numeric.
-        groupByColumn, # string containing the column to key by
-        aggregationColumn, # string containing the column to aggregate over.
-        outputColumnName = "quantiles",
-        quantiles = [0.5], # Python list containing the quantiles to compute
-        epsilon = 0.01
-    ):  # no force_type here because the Scala casts everything to Double whether you like it or not.
-        gk = self.gk_agg(
-            #self.sparkSession.sparkContext,
-            quantiles,
-            epsilon
         )
-        return df.groupBy(col(groupByColumn)).agg(gk(col(aggregationColumn)).alias(outputColumnName))
+    return _gk
+
+# A convenience function that calls the above. I wrote these mostly to follow
+# the pattern in the Scala, and I wrote the Scala functions the way I did just
+# so that people who weren't using SQL wouldn't have to remember treeAggregate
+# syntax.
+def getGroupedQuantilesSQL(
+    df,                # DF with some key column that responds to GroupBy
+                       # and some value column that is numeric.
+    groupByColumn,     # string containing the column to key by
+    aggregationColumn, # string containing the column to aggregate over.
+    outputColumnName = "quantiles", # string containing the
+    quantiles = [0.5], # Python list containing the quantiles to compute
+    epsilon = 0.01
+):  # no force_type here because the Scala casts everything to Double whether you like it or not.
+    gk = approximateQuantile(
+        quantiles,
+        epsilon
+    )
+    return df.groupBy(col(groupByColumn)).agg(gk(col(aggregationColumn)).alias(outputColumnName))
 
 
-
-    def getQuantiles(
-        self,
-        x, #RDD of some numeric type
-        quantiles, #list of numbers between 0 and 1
-        epsilon = 0.01,
-        force_type = float
-    ):
-        # force_type can be float, int, or None
-        #if force_type not in {float, int, None}:
-
-        if force_type is None:
-            inferred_type = type(x.first())
-            if inferred_type is float:
-                gq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesDouble
-            elif inferred_type is int:
-                gq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesInt
-            else:
-                raise Exception("couldn't figure out what to do with type {}".format(inferred_type))
-            x = self.py2java(x)
-        elif force_type is int:
-            gq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesInt
-            x = self.py2java(x.map(lambda y: int(y)))
-        elif force_type is float: # force_type is float, beccause we've already
-            gq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesDouble
-            x = self.py2java(x.map(lambda y: float(y)))
+# Take calculate quantiles over an RDD of
+# numbers, with no groupBy performed.
+# It can attempt to infer and preserve types if you pass force_type=None, or
+# it can force everything to be an int or a float.
+# This returns a *list* where the nth element corresponds to the
+# nth element of the quantiles argument.
+# TODO: make the output a dict (this involved changing the scala).
+def getQuantiles(
+    x,         #RDD of some numeric type
+    quantiles, #list of numbers between 0 and 1
+    epsilon = 0.01,
+    force_type = float
+):
+    # force_type can be float, int, or None
+    #if force_type not in {float, int, None}:
+    sc = SparkContext._active_spark_context
+    if force_type is None:
+        inferred_type = type(x.first())
+        if inferred_type is float:
+            gq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesDouble
+        elif inferred_type is int:
+            gq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesInt
         else:
-            raise Exception("We can either force everything to be a float, an int, or do basic type inspection.")
-        eps = self.py2java(epsilon)
-        q = self.py2java(quantiles)
-        return self.java2py(gq(x, q, eps))
-    def getGroupedQuantiles(
-        self,
-        x, #RDD of key-value pairs, where
-           # the key MUST be a string
-           # the value can be an int or a float
-        quantiles, # list of numbers between 0 and 1,
-        epsilon=0.01,
-        force_type = float,
-    ):
-        '''
-        So, this wound up being a huge pain because I didn't know what I was doing.
-        Here's the big idea: py4j treats python tuples the same way it treats python lists,
-        so it turns them into Arrays of primitive types. Since my Scala code works on Tuple2s,
-        it's necessary to unpack those arrays into tuples before you do any Scala on them, and
-        it's necessary to turn tuples back into arrays before pulling it back into the python.
+            raise Exception("couldn't figure out what to do with type {}".format(inferred_type))
+        x = py2java(x)
+    elif force_type is int:
+        gq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesInt
+        x = py2java(x.map(lambda y: int(y)))
+    elif force_type is float: # force_type is float, beccause we've already
+        gq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetQuantilesDouble
+        x = py2java(x.map(lambda y: float(y)))
+    else:
+        raise Exception("We can either force everything to be a float, an int, or do basic type inspection.")
+    eps = py2java(epsilon)
+    q = py2java(quantiles)
+    return java2py(gq(x, q, eps))
 
-        If encode_keys is False, the keys MUST be a string.
-        '''
-        # there may be a problem with this; it fails a unit test when encode_keys is True
-        # taking it out for now. Beware!
-        # encode_keys = False
-        # if encode_keys is True:
-        #     key_map = x.keys().distinct().map(lambda y: (y, json.dumps(y)))
-        #     reverse_key_map = key_map.map(lambda y: (y[1], y[0]))
-        #     # swap out the old key with the new one.
-        #     x = x.join(key_map).map(
-        #         lambda y: (y[1][1], y[1][0])
-        #     )
-
-        if force_type is None:
-            inferred_type = type(x.first()[1])
-            if inferred_type is float:
-                ggq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesDouble
-            elif inferred_type is int:
-                ggq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesInt
-            else:
-                raise Exception("couldn't figure out what to do with type {}".format(inferred_type))
-            x = self.py2java(x)
-        elif force_type is int:
-            ggq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesInt
-            #x = self.py2java(x.mapValues(lambda x: int(x)))
-            x = self.py2java(x.mapValues(lambda x: int(x)))
-        elif force_type is float:
-            ggq = self.sparkContext._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesDouble
-            x = self.py2java(x.mapValues(lambda x: float(x)))
-            #x = x.mapValues(lambda x: float(x))
+# Analogous to above, but instead of working on an RDD of numbers, it
+# works on an RDD of key/value pairs, where the key a string
+# (other types may work, but I can't guarantee that e.g. tuples will work because
+# of the way Python objects get passed into java.
+# this returns an RDD
+def getGroupedQuantiles(
+    x, #RDD of key-value pairs, where
+       # the key MUST be a string or something else that BOTH
+       # + goes through _py2java without turning into anything weird AND
+       # + is amenable to being a key for a key/value RDD.
+       # the value can be an int or a float
+    quantiles, # list of numbers between 0 and 1,
+    epsilon=0.01,
+    force_type = float,
+):
+    '''
+    So, this wound up being a huge pain because I didn't know what I was doing.
+    Here's the big idea: py4j treats python tuples the same way it treats python lists,
+    so it turns them into Arrays of primitive types. Since my Scala code works on Tuple2s,
+    it's necessary to unpack those arrays into tuples before you do any Scala on them, and
+    it's necessary to turn tuples back into arrays before pulling it back into the python.
+    '''
+    sc = SparkContext._active_spark_context
+    if force_type is None:
+        inferred_type = type(x.first()[1])
+        if inferred_type is float:
+            ggq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesDouble
+        elif inferred_type is int:
+            ggq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesInt
         else:
-            raise Exception("We can either force every value to be a float, an int, or do basic type inspection.")
+            raise Exception("couldn't figure out what to do with type {}".format(inferred_type))
+        x = py2java(x)
+    elif force_type is int:
+        ggq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesInt
+        #x = self.py2java(x.mapValues(lambda x: int(x)))
+        x = py2java(x.mapValues(lambda x: int(x)))
+    elif force_type is float:
+        ggq = sc._jvm.com.github.nlzimmerman.GKQuantile._PyGetGroupedQuantilesDouble
+        x = py2java(x.mapValues(lambda x: float(x)))
+        #x = x.mapValues(lambda x: float(x))
+    else:
+        raise Exception("We can either force every value to be a float, an int, or do basic type inspection.")
 
-        eps = self.py2java(epsilon)
-        q = self.py2java(quantiles)
-        out = self.java2py(ggq(x, q, eps))
-        # if we encoded the keys, now we have to get them back.
-        # if encode_keys is True:
-        #     out = out.map(lambda x: (x[0][0], (x[0][1], x[1]))).join(reverse_key_map).map(
-        #         lambda x: ((x[1][1], x[1][0][0]), x[1][0][1])
-        #     )
-        return out
+    eps = py2java(epsilon)
+    q = py2java(quantiles)
+    out = java2py(ggq(x, q, eps))
+
+    return out
--- python/test/testGK.py
+++ python/test/testGK.py
@@ -17,7 +17,6 @@ class BasicTest(unittest.TestCase):
                     config("spark.jars", "../target/scala-2.11/greenwald-khanna-udaf_2.11-0.0.1.jar").
                     getOrCreate()
                 )
-        self.g = GKQuantile()
         self.a = self.sparkSession.sparkContext.parallelize([1.0,2.0,3.0,4.0,5.0])
         self.b = self.sparkSession.sparkContext.parallelize([10,20,30,40,50])
         self.normal = NormalNumbers()
@@ -29,18 +28,18 @@ class BasicTest(unittest.TestCase):
             0.82,
             0.95
         ]
-        n0 = self.g.sparkContext.parallelize(self.normal.numbers, 100).map(lambda x: ("a", x))
-        n1 = self.g.sparkContext.parallelize(self.normal.numbers2, 100).map(lambda x: ("b", x))
+        n0 = self.sparkSession.sparkContext.parallelize(self.normal.numbers, 100).map(lambda x: ("a", x))
+        n1 = self.sparkSession.sparkContext.parallelize(self.normal.numbers2, 100).map(lambda x: ("b", x))
         self.labeled_normal_numbers = n0.union(n1).toDF(["name", "value"]).repartition(100).cache()
     def test_sanity(self):
         self.assertEqual(self.a.count(), 5)
     def test_quantile_float_simple(self):
-        x = self.g.getQuantiles(self.a, [0.5], force_type = None)
+        x = getQuantiles(self.a, [0.5], force_type = None)
         self.assertEqual(len(x), 1)
         self.assertEqual(type(x[0]), float)
         self.assertEqual(x[0], 3.0)
     def test_quantile_int_simple(self):
-        x = self.g.getQuantiles(self.b, [0.5], force_type = None)
+        x = getQuantiles(self.b, [0.5], force_type = None)
         self.assertEqual(len(x), 1)
         self.assertEqual(type(x[0]), int)
         self.assertEqual(x[0], 30)
@@ -52,7 +51,7 @@ class BasicTest(unittest.TestCase):
         for epsilon in [0.005, 0.01, 0.05]:
             bounds = Util.inverseNormalCDFBounds(self.targets, epsilon)
             # getQuantiles returns a LIST, not an RDD
-            n = self.g.getQuantiles(n0, self.targets, epsilon)
+            n = getQuantiles(n0, self.targets, epsilon)
             self.assertEqual(len(bounds), len(n))
             for b, x in zip(bounds, n):
                 self.assertTrue(b[0] <= x)
@@ -61,7 +60,7 @@ class BasicTest(unittest.TestCase):
         ''' inversion by key, NOT using Spark '''
         for epsilon in [0.005, 0.01, 0.05]:
             bounds = Util.inverseNormalCDFBounds(self.targets, epsilon)
-            quantiles = self.g.getGroupedQuantiles(
+            quantiles = getGroupedQuantiles(
                 self.labeled_normal_numbers.rdd.map(lambda x: (x['name'], x['value'])),
                 self.targets,
                 epsilon,
@@ -72,7 +71,7 @@ class BasicTest(unittest.TestCase):
                 (key, {t: b for t, b in zip(self.targets, bounds)})
                     for key in ("a", "b")
             ]
-            bounds_rdd = self.g.sparkContext.parallelize(bounds_list)
+            bounds_rdd = self.sparkSession.sparkContext.parallelize(bounds_list)
             # this takes as input a tuple,
             # with item 0 as the bounds (as a dict (quantile -> (lower, upper)))
             # and item 1 as the value (quantile -> value)
@@ -107,7 +106,7 @@ class BasicTest(unittest.TestCase):
                         Util.inverseNormalCDFBounds(self.targets, epsilon)
                     )
             }
-            quantiles_df = self.g.getGroupedQuantilesSQL(
+            quantiles_df = getGroupedQuantilesSQL(
                 self.labeled_normal_numbers,
                 "name",
                 "value",
@@ -129,7 +128,7 @@ class BasicTest(unittest.TestCase):
     def test_gk_agg(self):
         '''using gk_agg at epsilon=0.01'''
         #gk = self.g.gk_agg(self.sparkSession.sparkContext, self.targets, 0.01)
-        gk = self.g.gk_agg(self.targets, 0.01)
+        gk = approximateQuantile(self.targets, 0.01)
         quantiles_df = self.labeled_normal_numbers.groupBy(col("name")).agg(gk(col("value")).alias("quantiles"))
         quantiles_dict = quantiles_df.rdd.map(
             lambda x: (x["name"], x["quantiles"])
@@ -149,34 +148,34 @@ class BasicTest(unittest.TestCase):
                 self.assertTrue(
                     quantiles_dict[key][target] <= bounds[target][1]
                 )
-    def test_approximateQuantile(self):
-        '''using gk_agg at epsilon=0.01'''
-        aq = self.g.approximateQuantile(self.targets, 0.01)
-        quantiles_df = self.labeled_normal_numbers.groupBy(col("name")).agg(aq(col("value")).alias("quantiles"))
-        quantiles_dict = quantiles_df.rdd.map(
-            lambda x: (x["name"], x["quantiles"])
-        ).collectAsMap()
-        bounds = {
-            # quantile target: (lower bound, upper bound)
-            t: b for t, b in zip(
-                    self.targets,
-                    Util.inverseNormalCDFBounds(self.targets, 0.01)
-                )
-        }
-        for key in ["a", "b"]:
-            for target in self.targets:
-                self.assertTrue(
-                    bounds[target][0] <= quantiles_dict[key][target]
-                )
-                self.assertTrue(
-                    quantiles_dict[key][target] <= bounds[target][1]
-                )
+    # def test_approximateQuantile(self):
+    #     '''using gk_agg at epsilon=0.01'''
+    #     aq = self.g.approximateQuantile(self.targets, 0.01)
+    #     quantiles_df = self.labeled_normal_numbers.groupBy(col("name")).agg(aq(col("value")).alias("quantiles"))
+    #     quantiles_dict = quantiles_df.rdd.map(
+    #         lambda x: (x["name"], x["quantiles"])
+    #     ).collectAsMap()
+    #     bounds = {
+    #         # quantile target: (lower bound, upper bound)
+    #         t: b for t, b in zip(
+    #                 self.targets,
+    #                 Util.inverseNormalCDFBounds(self.targets, 0.01)
+    #             )
+    #     }
+    #     for key in ["a", "b"]:
+    #         for target in self.targets:
+    #             self.assertTrue(
+    #                 bounds[target][0] <= quantiles_dict[key][target]
+    #             )
+    #             self.assertTrue(
+    #                 quantiles_dict[key][target] <= bounds[target][1]
+    #             )
 
 
 
     def test_int_groupBy_spark_simple(self):
-        n0 = self.g.sparkContext.parallelize([0,1,2,3,4]).map(lambda x: ("foo", x))
-        q = self.g.getGroupedQuantiles(n0, [0.5], 0.01, force_type = None)
+        n0 = self.sparkSession.sparkContext.parallelize([0,1,2,3,4]).map(lambda x: ("foo", x))
+        q = getGroupedQuantiles(n0, [0.5], 0.01, force_type = None)
         x = q.collectAsMap()["foo"][0.5]
         self.assertEqual(x, 2)
         self.assertTrue(type(x) is int)
